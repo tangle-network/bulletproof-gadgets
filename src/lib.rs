56f306ffe2593f0ac6167f0980c59ef2b382e418
Shady Khalifa <shekohex@gmail.com>
2021-02-23 15:03:18 +0000

    extracted from anon

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[macro_use]
+extern crate alloc;
+
+pub mod crypto_constants;
+pub mod fixed_deposit_tree;
+pub mod poseidon;
+pub mod smt;
+pub mod time_based_rewarding;
+pub mod transaction;
+pub mod utils;
+pub mod variable_deposit_tree;
+pub mod zero_nonzero;
---
+++ src/smt/mod.rs
@@ -0,0 +1,7 @@
+pub mod builder;
+
+pub mod smt;
+pub use smt::*;
+
+#[cfg(test)]
+pub mod tests;
