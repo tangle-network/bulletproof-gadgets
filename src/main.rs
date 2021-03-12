bac7dba0ddfdf7c866be3f420d67ba77b5690807
Filip <lazoviccorp@gmail.com>
2021-03-12 15:07:37 +0100

    Fixed imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-extern crate curve25519_gadgets;
-use curve25519_dalek::scalar::Scalar;
+extern crate bulletproofs_gadgets;
 #[cfg(feature = "std")]
-use curve25519_gadgets::crypto_constants::utils::generate_zero_trees;
-use curve25519_gadgets::poseidon::{
+use bulletproofs_gadgets::crypto_constants::utils::generate_zero_trees;
+use bulletproofs_gadgets::poseidon::{
 	PoseidonBuilder, PoseidonSbox, Poseidon_hash_2,
 };
+use curve25519_dalek::scalar::Scalar;
 use std::{
 	env,
 	fs::{write, File},
